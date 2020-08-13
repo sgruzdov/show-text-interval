38019dc05e3c43cb03397a4e0052b8b75cfe5fce
Сергей Груздов <sergey_2342@tut.by>
2020-08-13 21:44:20 +0300

    Update show-text.interval.js

--- show-text.interval.js
+++ show-text.interval.js
@@ -1,11 +1,12 @@
-function showTextInterval({selector = '.text-interval', interval = 50}) {
+function showTextInterval({selector = '.text-interval', interval = 50, opacity = '1'}) {
     const arrSel = document.querySelectorAll(selector);
-
+    
     arrSel.forEach(elem => {
         let x = 0,
             str = elem.textContent;
 
         elem.textContent = '';
+        elem.style.opacity = opacity;
         const timer = setInterval(()=> { (x == str.length) ? clearInterval(timer) : elem.textContent += str[x]; x++ }, interval);
     })
 }
